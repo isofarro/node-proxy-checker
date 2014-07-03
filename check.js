5dff4ef8ed6729ba3ef21719c14a7bea59c0291e
Isofarro <isofarro2@gmail.com>
2014-07-03 15:36:41 +0000

    Example, bulk proxy checker

---
+++ check.js
@@ -0,0 +1,79 @@
+var checkProxy = require('./proxy-checker'),
+    fs         = require('fs'),
+    DELAY = 50,
+    stats = {
+        total: 0,
+        inProgress: 0,
+        pass: 0,
+        fail: 0
+    },
+    inputFile  = '',
+    outputFile = '',
+    output,
+    args = process.argv.slice(2),
+    proxyList,
+    listSize;
+
+
+if (args[0]) {
+    inputFile = args[0];
+}
+
+if (args[1]) {
+    output = fs.openSync(args[1], 'a');
+}
+
+
+proxyList = fs.readFileSync(inputFile, 'utf8').split('\n');
+listSize  = proxyList.length;
+
+
+var ticker = setInterval(function() {
+    proxy = proxyList.pop();
+    listSize = proxyList.length;
+
+    if (proxy) {
+        stats.total++;
+        stats.inProgress++;
+
+        checkProxy.check(proxy, {
+            pass: function(proxy) {
+                //console.log('Pass: ' + proxy);
+
+                if (output) {
+                    fs.write(output, proxy + "\n");
+                    process.stdout.write('#');
+                } else {
+                    console.log(proxy);
+                }
+
+                stats.pass++;
+                stats.inProgress--;
+            },
+            fail: function(proxy) {
+                //process.stdout.write('.');
+
+                if (output) {
+                    process.stdout.write('.');
+                }
+
+                stats.fail++;
+                stats.inProgress--;
+            }
+        });
+
+    } else if (stats.inProgress || listSize) {
+        // Keep ticking as proxy checks are in progress
+    } else {
+        clearInterval(ticker);
+        delete stats.inProgress;
+
+        if (output) {
+            fs.closeSync(output);
+            console.log('');
+            console.log(stats);
+        }
+    }
+}, DELAY);
+
+
